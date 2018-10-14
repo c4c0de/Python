c61dc50ccffc27ca105b41f8a3203542bfed137d
c4c0de <43881598+c4c0de@users.noreply.github.com>
2018-10-14 18:29:23 +0530

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,16 +1,6 @@
 # Requirements:
 #     pip install numpy
-#     sudo apt-get install python-openCV
-
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture(0)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
+#     sudo apt-get install python-ope
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
